0f3dcb48cea32c9b1f1b78111b494fc08834f48f
The Erin Collins & Peter <student@epicodus.com>
2015-05-26 10:18:55 -0700

    returns factorials of all positive numbers and zero

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 var factorial = function(inputNumber) {
   var resultFactorial = 1
-  for (var index = inputNumber; index > 0; index -= 1){
+  if (inputNumber === 0) {
+    return 1;
+  } else {
+  for (var index = inputNumber; index > 0; index -= 1) {
       resultFactorial *= index;
     }
     return resultFactorial;
+  }
 };
