8c3abc20170dbc355b76f8bdc3f0815f68fbf5c7
The Erin Collins & Peter <student@epicodus.com>
2015-05-26 10:14:22 -0700

    return non zero factorials

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 var factorial = function(inputNumber) {
-  return inputNumber;
+  var resultFactorial = 1
+  for (var index = inputNumber; index > 0; index -= 1){
+      resultFactorial *= index;
+    }
+    return resultFactorial;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,14 @@
 describe("factorial", function(){
   it('gives the factorial of 1', function(){
     expect(factorial(1)).to.equal(1);
+  });
 
+  it('returns the factorial of 3', function(){
+    expect(factorial(3)).to.equal(6);
+  });
+
+  it('returns 1 as the factorial of 0', function(){
+    expect(factorial(0)).to.equal(1);
   });
 
 
