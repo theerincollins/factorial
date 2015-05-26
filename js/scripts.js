5cfdfbbd1869df59faad1ba4cce6689762b5077d
The Erin Collins & Peter <student@epicodus.com>
2015-05-26 11:43:42 -0700

    refactor with recursion

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,12 @@
 var factorial = function(inputNumber) {
-  var resultFactorial = 1
+
   if (inputNumber === 0) {
     return 1;
-  } else {
-  for (var index = inputNumber; index > 0; index -= 1) {
-      resultFactorial *= index;
-    }
-    return resultFactorial;
   }
+
+  return inputNumber * factorial(inputNumber - 1));
+
+
 };
 
 
@@ -22,3 +21,8 @@ $(document).ready(function() {
     event.preventDefault();
   });
 });
+
+// for (var index = inputNumber; index > 0; index -= 1) {
+//     resultFactorial *= index;
+//   }
+//   return resultFactorial;
